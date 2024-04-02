bedaa982032a6d1112dbae39190cfe347d00ec58
sigehiro <sigehiro.0728@gmail.com>
2024-04-02 00:06:17 -0400

    1st commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const hideBtn = document.getElementById("hideBtn")
+const showBtn = document.getElementById("showBtn")
+const toggleBtn = document.getElementById("toggleBtn")
+const hoverText = document.getElementById("hoverText")
+const clickText = document.getElementById("clickText")
+
+const CapitalName = document.querySelector(".CapitalName")
+// const clickText = document.querySelectorAll("h3")[3]
+
+console.log(clickText);
+
+const hbtn  = () =>{
+    CapitalName.style.display = "none"
+}
+hideBtn.addEventListener("click", hbtn)
+
+const sbtn  = () =>{
+    CapitalName.style.display = "block"
+}
+showBtn.addEventListener("click", sbtn)
+
+const tgbtn  = () =>{
+    if(CapitalName.style.display === "none"){
+        CapitalName.style.display = "block"
+    }else{
+        CapitalName.style.display = "none"
+    }
+}
+toggleBtn.addEventListener("click", tgbtn)
+
+hoverText.addEventListener("mouseover", () =>{
+    hoverText.classList.add("hover-yellow")
+})
+
+hoverText.addEventListener("mouseout", () => {
+    hoverText.classList.remove("hover-yellow");
+});
+
+clickText.addEventListener("click", () =>{
+    clickText.classList.add("click-green")
+})
